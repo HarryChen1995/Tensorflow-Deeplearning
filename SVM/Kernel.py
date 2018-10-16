dd1a0e5adc6fff2c10f0d93c60d2f4da316e4872
Harry Chen <40048217+HarryChen1995@users.noreply.github.com>
2018-10-16 17:23:36 -0400

    Add files via upload

---
+++ SVM/Kernel.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+from sklearn.svm import SVC
+from plot_decision_regions import*
+
+np.random.seed(0)
+X_xor = np.random.randn(200, 2)
+y_xor = np.logical_xor(X_xor[:, 0] > 0, X_xor[:, 1] > 0)
+y_xor = np.where(y_xor, 1, -1)
+
+
+
+svm = SVC(kernel='rbf', random_state=0, gamma=0.1, C=10)
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor, classifier=svm)
+plt.legend(loc='upper left')
+plt.show()
